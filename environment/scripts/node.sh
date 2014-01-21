ed102aceaff93965cd6dd63f5ccb0780f4b85fc6
Daniel Hensby <dhensby@users.noreply.github.com>
2014-01-21 11:07:46 +0000

    UPDATE node.sh
    Removing sudo, all scripts should be run as root

--- environment/scripts/node.sh
+++ environment/scripts/node.sh
@@ -4,6 +4,6 @@ echo "Installing NodeJS (CentOS method)"
 echo "Downloading Ferodra EPEL repo"
 curl -O http://download-i2.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 echo "Installing EPEL"
-sudo rpm -ivh epel-release-6-8.noarch.rpm
+rpm -ivh epel-release-6-8.noarch.rpm
 echo "Installing NodeJS + NPM with EPEL"
-sudo yum install -y npm --enablerepo=epel
+yum install -y npm --enablerepo=epel
