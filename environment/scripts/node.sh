3305f21d022d993de759ae3d4db25a8eee53fce2
Daniel Hensby <dan@hens.by>
2014-01-21 14:15:25 +0000

    Cleaning up scripts
    node leaves some files around, so remove them after completion
    php-apc can be sped up

--- environment/scripts/node.sh
+++ environment/scripts/node.sh
@@ -7,3 +7,4 @@ echo "Installing EPEL"
 rpm -ivh epel-release-6-8.noarch.rpm
 echo "Installing NodeJS + NPM with EPEL"
 yum install -y npm --enablerepo=epel
+rm -f epel-release-6-8.noarch.rpm
--- environment/scripts/php-apc.sh
+++ environment/scripts/php-apc.sh
@@ -4,7 +4,8 @@ echo "Installing APC for php"
 yum install -y php-pear php-devel httpd-devel pcre-devel gcc make
 
 echo "Installing apc module for PHP"
-pecl install apc
+# printf helps skip the prompts from the installer - http://stackoverflow.com/a/8154466
+printf "\n" | pecl install apc
 
 echo "Adding APC to php config"
 echo "extension = apc.so" >> /etc/php.d/apc.ini
