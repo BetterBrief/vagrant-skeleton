41653d3db9beddaa1d161e8b6003f33283c20a39
Will Morgan <me@willmorgan.co.uk>
2014-01-21 13:24:40 +0000

    grunt + grunt-watch robustness changes
    On bootstrap run grunt default --force to stop error messages
    terminating the bootstrap
    grunt-watch install runs from the same working directory so the
    execution works correctly

--- environment/scripts/grunt-watch.sh
+++ environment/scripts/grunt-watch.sh
@@ -5,7 +5,7 @@ echo "Setting up grunt watch startup script (depends on grunt.sh)"
 if [ -r "/var/www/html/package.json" ]
 then
 	echo "Adding startup script 'grunt-watch.sh' to /etc/init.d";
-	echo "cd /vagrant/www && sudo nohup grunt watch > /dev/null 2> /dev/null < /dev/null &" > /etc/init.d/grunt-watch.sh
+	echo "cd /var/www/html && sudo nohup grunt watch > /dev/null 2> /dev/null < /dev/null &" > /etc/init.d/grunt-watch.sh
 	echo "Running grunt-watch.sh";
 	chmod +x /etc/init.d/grunt-watch.sh
 	/etc/init.d/grunt-watch.sh
