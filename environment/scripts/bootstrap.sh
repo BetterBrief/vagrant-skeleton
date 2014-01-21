a055498f7cdf4386fbdcdc3a5a79565e9be7bb83
Daniel Hensby <dhensby@users.noreply.github.com>
2014-01-21 10:49:40 +0000

    UPDATE bootstrap.sh
    Removing redundant comments

--- environment/scripts/bootstrap.sh
+++ environment/scripts/bootstrap.sh
@@ -2,10 +2,6 @@
 
 echo "Starting BB bootstrap"
 
-#Open ports on iptables
-echo "Setting up default iptables"
-
-
 #Fixing slow curl requests (ipv6 resolving timeouts causing issue)
 #See: https://github.com/mitchellh/vagrant/issues/1172
 if [ ! $(grep single-request-reopen /etc/resolv.conf) ];
