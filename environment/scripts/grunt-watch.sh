319d5142858077ff781c9664439e49f16743612b
Daniel Hensby <dhensby@users.noreply.github.com>
2014-01-21 10:55:09 +0000

    UPDATE grunt-watch.sh
    Removing sudo as scripts should be run as root
    Removing needles `cd`
    Changing location of package.json to be `/var/www/html` as not all webroots will be `/vagrant/www`

--- environment/scripts/grunt-watch.sh
+++ environment/scripts/grunt-watch.sh
@@ -2,14 +2,13 @@
 
 echo "Setting up grunt watch startup script (depends on grunt.sh)"
 
-if [ -r "/vagrant/www/package.json" ]
+if [ -r "/var/www/html/package.json" ]
 then
 	echo "Adding startup script 'grunt-watch.sh' to /etc/init.d";
-	sudo echo "cd /vagrant/www && sudo nohup grunt watch > /dev/null 2> /dev/null < /dev/null &" > /etc/init.d/grunt-watch.sh
+	echo "cd /vagrant/www && sudo nohup grunt watch > /dev/null 2> /dev/null < /dev/null &" > /etc/init.d/grunt-watch.sh
 	echo "Running grunt-watch.sh";
-	cd /etc/init.d
 	chmod +x /etc/init.d/grunt-watch.sh
-	./grunt-watch.sh
+	/etc/init.d/grunt-watch.sh
 	echo "grunt-watch.sh installed"
 	echo "NOTICE: If watch tasks are changed, these will take effect from the next time this machine is reloaded. You can test changed watch tasks with grunt watch in the terminal, however."
 else
