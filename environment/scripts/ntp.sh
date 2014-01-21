c1c808244f03ea7aa858c61ddd24600830218eb9
Daniel Hensby <dhensby@users.noreply.github.com>
2014-01-21 10:40:33 +0000

    Replacing ntp installer
    Taken the install script from https://github.com/qrpike/CentOS-6-Quick-Install-Scripts
    
    Many thanks

--- environment/scripts/ntp.sh
+++ environment/scripts/ntp.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-echo "Installing ntp"
+echo 'installing and configuring NTP'
 
-yum install -y ntp
-
-echo "Syncing ntp"
-
-ntpdate europe.pool.ntp.org
-
-echo "Adding ntpd to startup services"
-
+yum install -y wget ntp
 chkconfig ntpd on
+ntpdate pool.ntp.org
+/etc/init.d/ntpd start
 
-echo "Starting ntpd"
 
-service ntpd start
+# Set timezone
+rm -f /etc/localtime
+ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
 
-echo "Done install ntp"
+
+echo 'NTP is setup and configured'
+echo 'Below should be the correct date:'
+date
+
+# Replace centos pool with regular NTP pool
+sed -i 's/centos.pool.ntp.org/pool.ntp.org iburst/g' /etc/ntp.conf
+
+# Add this to not fail on big time gaps ( VMs that resume/pause )
+echo "tinker panic 0" >> /etc/ntp.conf 
+
+# Create a script to update time. 
+cat >/usr/bin/updatetime <<EOL
+/etc/init.d/ntpd stop
+ntpdate pool.ntp.org
+/etc/init.d/ntpd start
+EOL
+
+# now we can just run "updatetime" to restart and sync time servers:
+chmod +x /usr/bin/updatetime
