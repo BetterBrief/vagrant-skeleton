d3d70d894c77c31b5667459e58d9352e1df73bc0
Will Morgan <me@willmorgan.co.uk>
2014-01-21 13:23:04 +0000

    Add SASS

---
+++ environment/scripts/sass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing SASS"
+echo "Ensuring Ruby (dependency) is installed and up to date"
+yum install -y ruby
+echo "Ruby check complete. Version: `ruby --version`"
+gem install sass
+echo "Installed SASS version: `sass -v`"
