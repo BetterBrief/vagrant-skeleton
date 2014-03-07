6ccf7303fd2fca055c99ec4ffa041a8a0d35a088
Will Morgan <willmorgan@users.noreply.github.com>
2014-03-07 10:38:13 +0000

    Install SASS version 3.2.10
    Any version higher than this breaks due to "text file busy" errors. This is probably due to vagrant's shared folders.

--- environment/scripts/sass.sh
+++ environment/scripts/sass.sh
@@ -4,5 +4,5 @@ echo "Installing SASS"
 echo "Ensuring Ruby (dependency) is installed and up to date"
 yum install -y ruby rubygems
 echo "Ruby check complete. Version: `ruby --version`"
-gem install sass
+gem install sass -v 3.2.10
 echo "Installed SASS version: `sass -v`"
