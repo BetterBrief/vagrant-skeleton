3d00cf844c8cd87d2926a6ab7f6dce6e359e2c2c
Will Morgan <willmorgan@users.noreply.github.com>
2014-03-07 02:36:23 +0000

    Install rubygems too
    We need gems to install sass

--- environment/scripts/sass.sh
+++ environment/scripts/sass.sh
@@ -2,7 +2,7 @@
 
 echo "Installing SASS"
 echo "Ensuring Ruby (dependency) is installed and up to date"
-yum install -y ruby
+yum install -y ruby rubygems
 echo "Ruby check complete. Version: `ruby --version`"
 gem install sass
 echo "Installed SASS version: `sass -v`"
