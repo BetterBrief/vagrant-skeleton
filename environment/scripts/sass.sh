fc840bab9318b47fd5b1da8c9edeae468fdc3865
Dave Hill <dave.hill+git@betterbrief.co.uk>
2014-03-19 12:20:08 +0000

    Upgraded to latest SASS
    Added the ruby-devel package so we can compile rb-inotify and install
    the latest SASS.

--- environment/scripts/sass.sh
+++ environment/scripts/sass.sh
@@ -2,7 +2,8 @@
 
 echo "Installing SASS"
 echo "Ensuring Ruby (dependency) is installed and up to date"
-yum install -y ruby rubygems
+yum install -y ruby ruby-devel rubygems
 echo "Ruby check complete. Version: `ruby --version`"
-gem install sass -v 3.2.10
+gem install rb-inotify
+gem install sass
 echo "Installed SASS version: `sass -v`"
