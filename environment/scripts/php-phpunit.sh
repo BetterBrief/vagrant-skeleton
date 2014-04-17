d6a2664d008e2ffa95b322596ad12521c62a997c
Will Morgan <willmorgan@users.noreply.github.com>
2014-04-17 16:13:25 +0100

    Lock PHPUnit version to 3.7.34

--- environment/scripts/php-phpunit.sh
+++ environment/scripts/php-phpunit.sh
@@ -7,7 +7,7 @@ echo "Adding Pear channels"
 pear channel-discover pear.phpunit.de
 pear channel-discover pear.symfony.com
 
-echo "Installing PHPUnit - note this might take a while"
-pear install phpunit/PHPUnit
+echo "Installing PHPUnit 3.7.34 - note this might take a while"
+pear install phpunit/PHPUnit-3.7.34
 
 echo "PHPUnit and Pear installed"
