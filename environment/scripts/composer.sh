fceffd9bd2821f66cc1aa32251a87204c92f0e06
Will Morgan <me@willmorgan.co.uk>
2015-01-11 00:33:14 +0000

    Adding composer's vendor directory to system PATH

--- environment/scripts/composer.sh
+++ environment/scripts/composer.sh
@@ -7,6 +7,14 @@ curl -sS https://getcomposer.org/installer | php
 echo "Moving composer to make it globally accessable"
 mv composer.phar /usr/bin/composer
 
+echo "Adding composer's vendor directory to system PATH"
+cat >/etc/profile.d/composer-bin-root.sh <<EOF
+#!/bin/bash
+
+pathmunge /home/vagrant/.composer/vendor/bin after
+pathmunge /root/.composer/vendor/bin after
+EOF
+
 echo "Composer installed"
 
 echo "Installing git due to composer dependency"
