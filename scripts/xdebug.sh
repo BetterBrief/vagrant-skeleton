900a26b41815afae1fc7334fc642d12bacc2a8d1
Daniel Hensby <dhensby@silverstripe.com>
2015-06-28 06:28:56 +0100

    Improvements to xdebug

--- scripts/xdebug.sh
+++ scripts/xdebug.sh
@@ -7,7 +7,7 @@
 # 4. Apply and close settings
 # 5. Go to Run => Edit configurations
 # 6. Add a new PHP Web Application with the server you just added. The rest is trivial and free to fill.
-# 7. Aply and done!
+# 7. Apply and done!
 
 echo "Install Xdebug"
 yum install -y gcc gcc-c++ autoconf automake
@@ -17,6 +17,18 @@ if [ ! -f /etc/php.d/xdebug.ini ]
 then
     echo "[xdebug]
     zend_extension=\"/usr/lib64/php/modules/xdebug.so\"
+    xdebug.profiler_enable = 1
+    xdebug.profiler_enable_trigger = 1
+    xdebug.profiler_output_dir = \"/vagrant/profiling\"
     xdebug.remote_connect_back = 1
     xdebug.remote_enable = 1" > /etc/php.d/xdebug.ini
 fi
+
+if [ ! -d /vagrant/profiling ]; then
+	mkdir /vagrant/profiling
+fi
+
+#we need a bit longer to execute with profiling
+sed -i 's/max_execution_time.*/max_execution_time = 120/g' /etc/php.ini
+
+systemctl restart httpd
