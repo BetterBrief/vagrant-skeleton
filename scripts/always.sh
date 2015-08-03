62fb87b7309eb256bca0a3e4951adf3592d1c4a1
Daniel Hensby <dhensby@silverstripe.com>
2015-08-03 15:10:02 +0100

    Clearing SS cache on provision

--- scripts/always.sh
+++ scripts/always.sh
@@ -4,5 +4,11 @@ echo "starting httpd"
 systemctl start httpd.service
 
 if [ -x /usr/local/bin/mailcatcher ]; then
+	echo "Starting mailcatcher"
 	/usr/local/bin/mailcatcher --ip=0.0.0.0
+fi
+
+if [ -d /var/www/html/silverstripe-cache ]; then
+	echo "Clearing SS site cache"
+    rm -rf /var/www/html/silverstripe-cache/*
 fi
