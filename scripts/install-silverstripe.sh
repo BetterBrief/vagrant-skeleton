771994174f10bbebcca5c19dc248c8e5e52f62c7
Daniel Hensby <dhensby@silverstripe.com>
2015-08-04 12:03:54 +0100

    Cleaning up silverstripe installer

--- scripts/install-silverstripe.sh
+++ scripts/install-silverstripe.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-INSTALL_DIR=/vagrant/www/
-INSTALL_DESTINATION=/var/www/html
-INSTALL_VERSION=''
-
-INSTALLER_NAME='silverstripe/installer'
+INSTALL_DIR=/vagrant/www/ # The directory to install silverstripe into
+INSTALL_DESTINATION=/var/www/html # The directory to symlink the install dir to. Pass an empty -w flag to avoid symlink
+INSTALL_VERSION='' # The version constraint we want, by default it will be latest stable
+INSTALLER_NAME='silverstripe/installer' # The composer installer name, this could be changed to a custom one if needed
 
 function usage() {
 	echo -e "Syntax `basename $0` [-h] [-v version] [-d directory] [-i installer] [-w webroot]
 	-h Show this help
-	-v Set the version constraint for the isntaller
+	-v Set the version constraint for the installer
 	-d Set the directory to install into
-	-i Set the installer\n
-	-w Set the webroot to symlink"
+	-i Set the installer
+	-w Set the webroot to symlink\n"
 }
 
 function install() {
